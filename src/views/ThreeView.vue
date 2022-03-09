6e35f0be37955fa63a06ee624378492b19f167c8
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-03-09 20:32:55 +0900

    選択画面をv-forで書き換えました。

--- src/views/ThreeView.vue
+++ src/views/ThreeView.vue
@@ -4,35 +4,53 @@
     <p class="title__text">-コードを書いて例題のイメージに近づけよう！</p>
   </div>
   <div class="content__wrapper">
-    <div class="content__container">
+    <div
+      class="content__container"
+      v-for="content in contents"
+      v-bind:key="content.id"
+    >
       <img
         class="content__img"
-        src="@/assets/MaruLv1.jpg"
-        alt="図形の丸です。"
+        v-bind:src="content.imagePath"
+        v-bind:alt="content.imageAlt"
       />
-      <p class="content__text">まる</p>
-      <p class="content__record">前回の記録:〇分〇〇秒</p>
-    </div>
-    <div class="content__container">
-      <img
-        class="content__img"
-        src="@/assets/SankakuLv1.jpg"
-        alt="図形の丸です。"
-      />
-      <p class="content__text">さんかく</p>
-      <p class="content__record">前回の記録:〇分〇〇秒</p>
-    </div>
-    <div class="content__container">
-      <img
-        class="content__img"
-        src="@/assets/DaikeiLv1.jpg"
-        alt="図形の丸です。"
-      />
-      <p class="content__text">だいけい</p>
-      <p class="content__record">前回の記録:〇分〇〇秒</p>
+      <p class="content__text">{{ content.text }}</p>
+      <p class="content__record">{{ content.record }}</p>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      contents: [
+        {
+          id: 0,
+          imagePath: require("@/assets/MaruLv1.jpg"),
+          imageAlt: "図形の丸です。",
+          text: "まる",
+          record: "前回の記録:〇分〇〇秒",
+        },
+        {
+          id: 1,
+          imagePath: require("@/assets/SankakuLv1.jpg"),
+          imageAlt: "図形の三角です。",
+          text: "さんかく",
+          record: "前回の記録:〇分〇〇秒",
+        },
+        {
+          id: 2,
+          imagePath: require("@/assets/DaikeiLv1.jpg"),
+          imageAlt: "図形の台形です。",
+          text: "だいけい",
+          record: "前回の記録:〇分〇〇秒",
+        },
+      ],
+    }
+  },
+}
+</script>
 
 <style scoped>
 html {
@@ -60,11 +78,18 @@ html {
   display: flex;
   justify-content: center;
   border: 1px solid #333;
+  padding: 80px;
 }
 .content__container {
   border: 1px solid #333;
+  margin: 0 40px;
+  padding: 5px;
 }
 .content__img {
   border: 1px solid #333;
+}
+.content__text {
+  font-weight: bold;
+  font-size: 1.5rem;
 }
 </style>
