de265518d355c1f73a4c6a5388b2242e64d66cd8
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-03-09 19:44:12 +0900

    選択画面のレイアウトをざっと作成しました。

--- src/views/ThreeView.vue
+++ src/views/ThreeView.vue
@@ -1,5 +1,70 @@
 <template>
-  <div class="three">
-    <h1>This is a three page</h1>
+  <div class="title__container">
+    <h2 class="title__logo">CSS battle</h2>
+    <p class="title__text">-コードを書いて例題のイメージに近づけよう！</p>
+  </div>
+  <div class="content__wrapper">
+    <div class="content__container">
+      <img
+        class="content__img"
+        src="@/assets/MaruLv1.jpg"
+        alt="図形の丸です。"
+      />
+      <p class="content__text">まる</p>
+      <p class="content__record">前回の記録:〇分〇〇秒</p>
+    </div>
+    <div class="content__container">
+      <img
+        class="content__img"
+        src="@/assets/SankakuLv1.jpg"
+        alt="図形の丸です。"
+      />
+      <p class="content__text">さんかく</p>
+      <p class="content__record">前回の記録:〇分〇〇秒</p>
+    </div>
+    <div class="content__container">
+      <img
+        class="content__img"
+        src="@/assets/DaikeiLv1.jpg"
+        alt="図形の丸です。"
+      />
+      <p class="content__text">だいけい</p>
+      <p class="content__record">前回の記録:〇分〇〇秒</p>
+    </div>
   </div>
 </template>
+
+<style scoped>
+html {
+  box-sizing: border-box;
+}
+*,
+*::before,
+*::after {
+  box-sizing: inherit;
+}
+.title__container {
+  background-color: aquamarine;
+}
+.title__logo {
+  font-size: 3.5rem;
+  text-align: left;
+  margin-left: 30px;
+}
+.title__text {
+  font-size: 1.5rem;
+  text-align: left;
+  margin-left: 100px;
+}
+.content__wrapper {
+  display: flex;
+  justify-content: center;
+  border: 1px solid #333;
+}
+.content__container {
+  border: 1px solid #333;
+}
+.content__img {
+  border: 1px solid #333;
+}
+</style>
