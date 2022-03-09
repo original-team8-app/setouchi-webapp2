11d453cfee7c9582a64638bd120024d192c49c17
chisato-818 <chisa818.milkyyy@gmail.com>
2022-03-09 09:04:41 +0900

    初期設定

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
+
+const firebaseConfig = {
+  //...apiKey: "AIzaSyCwSc8wkBegdhVzxXgUJqGmnPuiQmGCUIM",
+  authDomain: "setouchi-web-app.firebaseapp.com",
+  projectId: "setouchi-web-app",
+  storageBucket: "setouchi-web-app.appspot.com",
+  messagingSenderId: "480976356052",
+  appId: "1:480976356052:web:e836ccb875360645643141",
+  measurementId: "G-CENMEQ3CLZ",
+}
+
+// Firebaseアプリオブジェクトを初期化
+const app = initializeApp(firebaseConfig)
+// Firestoreを読み込み、db(databaseの略)として export
+export const db = getFirestore(app)
