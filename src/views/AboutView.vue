52bc6ee29c3fb1a34a7c542a633ebe9e5ac422ca
Keiji-morita <keijim3116@gmail.com>
2022-03-09 18:23:53 +0900

    first page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,4 +2,20 @@
   <div class="about">
     <h1>CSSの使い方を覚えよう！</h1>
   </div>
+
+  <div class="ruleIntroduction">
+    <h2>ルール説明</h2>
+    <p>CSSでレイアウトを変更し、左画面にある車が目的地に到着させましょう！</p>
+  </div>
+  <div>
+    <img
+      src="../assets/img/0683-download-image-m.png"
+      alt="Logo"
+      class="carIcon"
+    />
+  </div>
+
+  <div class="staetbutton">
+    <button>ゲームを始める</button>
+  </div>
 </template>
