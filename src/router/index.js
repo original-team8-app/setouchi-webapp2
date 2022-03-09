b9a4416c746bfa2ff5144d9c40551cc4c005f450
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-03-09 09:28:04 +0900

    3つ目のrouterページを追加しました。

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router"
 import HomeView from "../views/HomeView.vue"
+import ThreeView from "../views/ThreeView.vue"
 
 const routes = [
   {
@@ -15,6 +16,11 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+  {
+    path: "/three",
+    name: "three",
+    component: ThreeView,
   },
 ]
 
