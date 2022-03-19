2953f626db88a1d6cb43e6642c35c5a03aa8c6a9
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-03-20 02:22:42 +0900

    documentの参照を実装しました。

--- src/views/RecordView.vue
+++ src/views/RecordView.vue
@@ -1,12 +1,41 @@
 <template>
-  <div>{{ deliveryDocName }}</div>
+  <div class="title">{{ title }}</div>
+  <div>
+    <div class="back" v-html="recordData.contentCode"></div>
+  </div>
 </template>
 
 <script>
+import { doc, getDoc } from "firebase/firestore"
+import { db } from "../firebase"
+
 export default {
   props: ["deliveryDocName"],
   data() {
-    return {}
+    return {
+      title: "前回の記録",
+      recordData: {},
+    }
+  },
+  created() {
+    getDoc(doc(db, "contents", this.deliveryDocName)).then((snapshot) => {
+      this.recordData = snapshot.data()
+    })
   },
 }
 </script>
+
+<style scoped>
+.title {
+  font-size: 2rem;
+}
+.back {
+  position: absolute;
+  top: 25vh;
+  left: 80vh;
+  height: 300px;
+  width: 300px;
+  outline: auto;
+  padding: 10px;
+}
+</style>
