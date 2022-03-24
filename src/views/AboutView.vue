2da34bef504d8a4a39a938821c18c2a3144c3581
Keiji-morita <keijim3116@gmail.com>
2022-03-24 19:01:00 +0900

    ページ遷移質問

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -22,7 +22,7 @@
 
     <div id="card__list">
       <router-link
-        to="/first-question.vue"
+        v-bind:to="menue.path"
         v-for="menue in menueData"
         v-bind:key="menue.title"
         class="menue__card"
