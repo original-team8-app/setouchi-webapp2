c2134b45b321540c0198204891df2eeff655811f
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-03-28 11:31:03 +0900

    TopViewのrouterとhoverを設定しました。

--- src/views/TopView.vue
+++ src/views/TopView.vue
@@ -4,14 +4,24 @@
       <div class="title">{{ title }}</div>
     </header>
     <main>
-      <div class="contents" v-for="content in contents" :key="content.index">
+      <div
+        class="contents"
+        v-for="content in contents"
+        :key="content.index"
+        @click="toGames(content.name)"
+      >
         <img
           class="contents__img"
           :src="content.imgPath"
           :alt="content.altText"
         />
         <div class="contents__text">
-          <p class="contents__mainTx">{{ content.mainTx }}</p>
+          <p
+            class="contents__mainTx"
+            :style="{ backgroundColor: content.bgColor }"
+          >
+            {{ content.mainTx }}
+          </p>
           <p class="contents__subTx">{{ content.subTx }}</p>
         </div>
       </div>
@@ -26,26 +36,37 @@ export default {
       title: "Setouchi Web App仮",
       contents: [
         {
+          name: "home",
           imgPath: require("@/assets/Img1.jpeg"),
           altText: "",
           mainTx: "Quiz",
+          bgColor: "rgba(255, 210, 120, 0.6)",
           subTx:
             "初級のクイズ。便利なショートカットキーがたくさん覚えられるよ。",
         },
         {
+          name: "about",
           imgPath: require("@/assets/Img1.jpeg"),
           altText: "",
           mainTx: "Game",
+          bgColor: "rgba(165, 220, 220, 0.6)",
           subTx: "中級のゲーム。flexboxの使い方について楽しく学ぼう！。",
         },
         {
+          name: "css-park",
           imgPath: require("@/assets/CSSParkImg.jpg"),
           altText: " CSS Park",
+          bgColor: "rgba(190, 235, 195, 0.6)",
           mainTx: "CSS Park",
           subTx: "上級「CSS Park」。イラストを作ってCSSの腕試しをしよう！",
         },
       ],
     }
+  },
+  methods: {
+    toGames(game) {
+      this.$router.push({ name: game })
+    },
   },
 }
 </script>
@@ -64,7 +85,7 @@ html {
 }
 @keyframes titleAnimation {
   0% {
-    letter-spacing: 0.3em;
+    letter-spacing: 0.1em;
     opacity: 0;
   }
 }
@@ -72,25 +93,45 @@ html {
   font-size: 5rem;
   font-weight: bold;
   display: inline-block;
-  margin: 30px auto 80px auto;
+  margin: 30px auto 120px auto;
   animation: titleAnimation 1.5s;
 }
+@keyframes mainAnimation {
+  0% {
+    transform: translateY(20px);
+    opacity: 0;
+  }
+  50% {
+    transform: translateY(20px);
+    opacity: 0;
+  }
+}
 main {
-  max-width: 900px;
+  max-width: 950px;
   width: 100%;
   margin: 0 auto;
+  transform: 0;
+  opacity: 1;
+  animation: mainAnimation 2.5s;
 }
 .contents {
   position: relative;
+  opacity: 0.8;
+  transition: all 0.3s;
+}
+.contents:hover {
+  opacity: 1;
+  transform: scale(1.02);
+  transition: all 0.6s;
 }
 .contents:not(:last-of-type) {
   margin-bottom: 200px;
 }
 .contents:nth-of-type(odd) .contents__img {
-  transform: translate(-200px, 0);
+  transform: translate(-180px, 0);
 }
 .contents:nth-of-type(even) .contents__img {
-  transform: translate(200px, 0);
+  transform: translate(180px, 0);
 }
 .contents:nth-of-type(odd) .contents__text {
   right: 0;
@@ -111,16 +152,14 @@ main {
 }
 .contents__mainTx {
   font-size: 3rem;
-  background-color: rgba(185, 230, 190, 0.8);
   padding: 10px 0;
   margin: 0 auto;
   width: 400px;
   border-radius: 5px;
-  /* letter-spacing: 5px; */
 }
 .contents__subTx {
   font-size: 1.5rem;
-  background-color: rgba(255, 228, 196, 0.8);
+  background-color: rgba(255, 255, 255, 0.8);
   padding: 10px 20px;
   width: 300px;
   border-radius: 5px;
