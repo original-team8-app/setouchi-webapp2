0e997e644f91961129bfaba0ad3722c8c3ce666a
KTaketora <carp.8.tora@gmail.com>
2022-03-29 10:20:53 +0900

    now

--- src/views/shortcut/HomeView.vue
+++ src/views/shortcut/HomeView.vue
@@ -1,57 +1,114 @@
 <template>
-  <div class="topBox">
-    <div class="title">
-      <h2>ShortCutQuiz(仮)</h2>
-      <div class="marker"></div>
+  <div class="container">
+    <div class="topBox">
+      <div class="title">
+        <h2 v-if="startFlag != true">ShortCutQuiz</h2>
+        <div class="marker"></div>
+      </div>
+      <p v-if="startFlag != true">
+        無人島に遭難してしまった！<br />島から脱出するには問題を解いて船を完成させよう！<br />
+      </p>
+      <img
+        src="@/assets/uninhabitedIsland.png"
+        alt="無人島で遭難中"
+        class="firstImg mb-20"
+        v-if="startFlag != true"
+      />
+      <div class="clear" v-if="completed">
+        <h4>脱出成功！！</h4>
+        <img
+          src="@/assets/escape.jpg"
+          alt="脱出成功！"
+          class="escapeImg"
+          v-if="completed"
+        />
+      </div>
+      <div class="notclear" v-if="completed">
+        <h4>脱出失敗…</h4>
+        <img
+          src="@/assets/oboreru.png"
+          alt="脱出失敗"
+          class="notescapeImg"
+          v-if="completed"
+        />
+      </div>
     </div>
-    <img src="" alt="無人島で遭難中" />
-    <p>木を集めて脱出を目指せ！</p>
-  </div>
-  <div v-if="startFlag != true" class="middleBox">
-    <button class="startButton" @click="gameStart">START</button>
-    <ul>
-      <a href="">説明</a>
-    </ul>
-    <p>これまでの最高スコア</p>
-  </div>
-  <div class="result" v-if="current_question_counts > 2">
-    <h2>結果</h2>
-    <p>〇問正解</p>
-    <img src="" alt="脱出成功！" />
-    <p>脱出成功！！</p>
-    <div class="jumpBox">
+    <div v-if="startFlag != true" class="middleBox">
+      <button class="startButton mb-20" @click="gameStart">GAME START</button>
+      <h4>-説明-</h4>
+      <p>
+        Visual Studio
+        Codeのショートカットをマスターして開発をスムーズに進めよう！<br />Visual
+        Studio
+        Code(VSCode)は多くのプログラミング言語に対応しているソースコードエディタだ。<br />このゲームでは覚えておくとプログラミング効率がUPするお役立ちショートカットをクイズで出題！<br />基本のショートカットからマニアックなものまで開発に欠かせないショートカットを厳選。<br />全15問で7問以上正解でクリアだ！
+      </p>
+    </div>
+    <!-- <div class="result" v-if="current_question_counts > 2">
+      <h2>結果</h2>
+      <p>〇問正解</p>
+      <img src="" alt="脱出成功！" />
+      <p>脱出成功！！</p> -->
+    <div class="offset-3 col-6" v-if="completed">
+      <div class="card">
+        <div class="card-body">
+          <p class="badge">結果</p>
+          <div v-for="(question, index) in this.questions" :key="question">
+            <h4 class="card-title">{{ question.question }}</h4>
+            <ul>
+              <li v-for="answer in question.answers" :key="answer">
+                {{ answer }}
+              </li>
+            </ul>
+            <span v-if="question.answer == answers[index]">正解 ⭕️ </span>
+            <span v-else
+              >不正解 ❌ <br />正解は「{{
+                question.answers[question.answer]
+              }}」でした。</span
+            >
+            <hr />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="jumpBox" v-if="completed">
       <button>リトライ</button>
       <button>解説</button>
       <button>タイトルへ</button>
     </div>
-  </div>
-  <div v-if="startFlag">
-    <div class="question mb-20">
-      {{ current_question }}
+    <div v-if="startFlag">
+      <div class="imgBox">
+        <div class="seikai">
+          <h4>正解！</h4>
+          <img src="@/assets/maruta.png" alt="正解" class="seikaiImg" />
+        </div>
+        <div class="zannen">
+          <h4>残念！</h4>
+          <img src="@/assets/basyabasya.jpg" alt="残念" class="zannenImg" />
+        </div>
+      </div>
+      <p class="badge badge-dark">第 {{ questionIndex + 1 }} 問</p>
+      <div class="question mb-20">
+        {{ currentQuestion.question }}
+      </div>
+      <div class="choice mb-20">
+        <button
+          v-bind:key="answer"
+          v-for="(answer, index) in currentQuestion.answers"
+          @click="multipulHandler(index)"
+        >
+          {{ index + 1 }}. {{ answer }}
+        </button>
+      </div>
+      <div class="keyboad">
+        <img src="@/assets/keyboad.png" alt="キーボード" class="keyboadImg" />
+      </div>
+      <div class="gaugeWrapper mb-20">
+        <div v-bind:style="styleObject" class="gauge"></div>
+      </div>
+      <div class="question-count">
+        {{ current_question_counts }} / {{ question_counts }}
+      </div>
     </div>
-    <div class="choice mb-20">
-      <button
-        v-for="select in selects"
-        v-bind:key="select.id"
-        v-on:click="clickButton(select)"
-      >
-        {{ select.text }}
-      </button>
-    </div>
-    <div class="gaugeWrapper mb-20">
-      <div v-bind:style="styleObject" class="gauge"></div>
-    </div>
-    <div class="question-count">
-      {{ current_question_counts }} / {{ question_counts }}
-    </div>
-  </div>
-  <div class="routerBox">
-    <ul>
-      <a href="">ホームへ</a>
-      <a href="">ゲーム2へ</a>
-      <a href="">ゲーム3へ</a>
-      <a href="">ゲーム4へ</a>
-    </ul>
   </div>
 </template>
 
@@ -67,63 +124,155 @@ export default {
       current_question: "",
       current_question_counts: 0,
       question_counts: 0,
-      questions: ["コピー", "ペースト", "全選択"],
-      selects: [
+      questionIndex: 0,
+      answers: [],
+      questions: [
         {
-          id: 0,
-          text: "Ctrl+K",
-          isCollect: false,
+          question: "選択範囲のコピー",
+          answers: ["「Ctrl」+「K」", "「Ctrl」+「C」", "「Alt」+「C」"],
+          answer: 1,
         },
         {
-          id: 1,
-          text: "Ctrl+C",
-          isCollect: true,
+          question: "選択範囲の貼り付け",
+          answers: ["「Ctrl」+「P」", "「Alt」+「V」", "「Ctrl」+「V」"],
+          answer: 2,
         },
         {
-          id: 2,
-          text: "Alt+C",
-          isCollect: false,
+          question: "すべて選択",
+          answers: ["「Ctrl」+「A」", "「Ctrl」+「Z」", "「Alt」+「A」"],
+          answer: 0,
+        },
+        {
+          question: "検索",
+          answers: ["「Ctrl」+「F」", "「Ctrl」+「K」", "「Alt」+「S」"],
+          answer: 0,
+        },
+        {
+          question: "末尾までの文章を選択",
+          answers: ["「Ctrl」+「E」", "「Ctrl」+「E」", "「Shift」+「End」"],
+          answer: 2,
+        },
+        {
+          question: "「Ctrl」+「X」",
+          answers: ["ズームイン", "選択範囲の切り取り", "選択行を削除"],
+          answer: 1,
+        },
+        {
+          question: "「Ctrl」+「Shift」+「P」",
+          answers: [
+            "ページ単位で上下に移動",
+            "カーソル行を下に移動",
+            "コマンドパレットを開く",
+          ],
+          answer: 2,
+        },
+        {
+          question: "「Ctrl」+「/」",
+          answers: [
+            "行をコメントアウト・コメント解除",
+            "単語単位でカーソル移動",
+            "選択部分の実装へ移動する",
+          ],
+          answer: 0,
+        },
+        {
+          question: "「Shift」+「Alt」+「F」",
+          answers: [
+            "前を検索",
+            "コードをきれいに整える",
+            "新しいファイルを開く",
+          ],
+          answer: 1,
+        },
+        {
+          question: "「Ctrl」+「|」",
+          answers: [
+            "選択部分の定義をプレビュー表示",
+            "文章の末尾にカーソル移動",
+            "画面の2分割",
+          ],
+          answer: 2,
         },
       ],
     }
   },
-  // computed: {
-  //   styleObject: function () {
-  //     width = 10 * this.current_question_counts + "%"
-  //     if (this.current_question_counts == 3) {
-  //       color = "#03a9f4"
-  //     } else {
-  //       color = "orange"
-  //     }
-  //     return {
-  //       width: "width",
-  //       color: "background-color",
-  //     }
-  //   },
-  // },
+  computed: {
+    // styleObject: function () {
+    //   width = 10 * this.current_question_counts + "%"
+    //   if (this.current_question_counts == 3) {
+    //     color = "#03a9f4"
+    //   } else {
+    //     color = "orange"
+    //   }
+    //   return {
+    //     width: "width",
+    //     color: "background-color",
+    //   }
+    // },
+    currentQuestion: function () {
+      return this.questions[this.questionIndex]
+    },
+    completed: function () {
+      return this.questions.length == this.answers.length
+    },
+  },
   methods: {
     gameStart: function () {
       this.startFlag = true
     },
-    clickButton: function (select) {
-      this.questions.splice(0, 1)
+    multipulHandler: function (index) {
+      this.clickButton()
+      this.addAnswer(index)
+    },
+    clickButton: function () {
       this.current_question = this.questions[0]
-      if (select.isCollect == true) {
+      if (this.questions.length == this.answers.length) {
         this.current_question_counts = this.current_question_counts + 1
       }
     },
-  },
-  mounted: function () {
-    this.current_question = this.questions[0]
-    this.question_counts = this.questions.length
+    addAnswer: function (index) {
+      this.answers.push(index)
+      if (!this.completed) {
+        this.questionIndex++
+      }
+    },
+    mounted: function () {
+      this.current_question = this.questions[0]
+      this.question_counts = this.questions.length
+    },
   },
 }
 </script>
 
 <style scoped>
+.topBox {
+  widows: 400px;
+  margin: 0 auto;
+  text-align: center;
+}
 .title {
   position: relative;
   font-size: 50px;
+}
+.firstImg {
+  width: 200px;
+}
+.escapeImg {
+  width: 200px;
+}
+.notescapeImg {
+  width: 200px;
+}
+.keyboadImg {
+  width: 1000px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+.seikaiImg {
+  width: 200px;
+}
+.zannenImg {
+  width: 200px;
 }
 .mb-20 {
   margin-bottom: 20px;
@@ -138,13 +287,14 @@ export default {
   z-index: -1;
 }
 .startButton {
-  background-color: #afcbff;
+  background-color: teal;
   color: #fff;
   padding: 4px 60px;
   border: none;
   outline: none;
-  border-radius: 8px;
+  border-radius: 20px;
   cursor: pointer;
+  height: 50px;
 }
 .startButton:hover {
   opacity: 0.7;
